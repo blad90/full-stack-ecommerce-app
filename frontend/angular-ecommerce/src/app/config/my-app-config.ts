78f05aa5151af0cde6276bf521461249e0e87a03
Blad <bladimirbaezt@gmail.com>
2024-08-26 11:45:31 -0400

    [NEW] add security access feature + other auth configurations

---
+++ frontend/angular-ecommerce/src/app/config/my-app-config.ts
@@ -0,0 +1,8 @@
+export default {
+  oidc: {
+    clientId: '0oaj4x3hphHZGzzzS5d7',
+    issuer: 'https://dev-40838009.okta.com/oauth2/default',
+    redirectUri: 'http://localhost:4200/login/callback',
+    scopes: ['openid', 'profile', 'email'],
+  },
+};
